214fabebc0f4c6c46fd0cc35b1bb977e82247c8d
Beilin Li <nix7bg2mj1@gmail.com>
2016-08-07 17:37:01 -0400

    change to EJS/Bootstrap

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('static'))
 
 app.set('port', (process.env.PORT || 3000))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('pages/index')
+})
+
 app.listen(app.get('port'), function() {
   console.log('App running on port ' + app.get('port'))
 })
