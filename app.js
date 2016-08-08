4e0974ff2d003b3c1a12168cb4c12228fe479796
Beilin Li <nix7bg2mj1@gmail.com>
2016-08-08 04:09:23 -0400

    homepage design

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ var bodyParser = require('body-parser')
 var app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('static'))
+app.use(express.static('public'))
 
 app.set('port', (process.env.PORT || 3000))
-app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
